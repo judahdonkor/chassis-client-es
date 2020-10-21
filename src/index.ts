ccf234d318d91652ac9235b115d2d1d349bf2b4b
Judah Donkor <judahdonkor@gmail.com>
2020-10-21 13:05:49 +0000

    Use better name

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { Repository } from './repository';
-import { Exchange } from './exchange';
 import { AxiosStatic } from 'axios';
-import { Creation } from './creation'
+import { Creation } from './creation';
+import { Exchange } from './exchange';
+import { Repository } from './repository';
 
 interface Enum {
   name: string;
@@ -14,17 +14,18 @@ class Chassis {
   private axios: AxiosStatic;
   repos: Repository;
   xchg: Exchange;
-  cr8: Creation;
+  creation: Creation;
   constructor(axios: AxiosStatic) {
     this.axios = axios;
     this.repos = new Repository(axios);
     this.xchg = new Exchange(axios);
-    this.cr8 = new Creation(axios);
+    this.creation = new Creation(axios);
   }
   async enum(cls: string): Promise<Enum[]> {
     return (await this.axios.get(`enum/${cls}`)).data;
   }
 }
 
-export type { Enum }
+export type { Enum };
 export { Repository, Chassis, Exchange };
+
